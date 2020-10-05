102cbf92470dda9df861cb90f20afd26ef916362
yoosungung <dulle2@gmail.com>
2020-10-05 08:30:40 +0000

    new file:   dailybuild.sh

---
+++ dailybuild.sh
@@ -0,0 +1,16 @@
+cd ~/microflow
+
+CURRENT_PID=$(pgrep -f microflow-1.0-SNAPSHOT-jar-with-dependencies.jar)
+if [ -z "$CURRENT_PID" ]; then
+	  echo "> Current Server is nothing"
+	else
+		  kill -15 $CURRENT_PID
+fi
+
+git pull
+
+mvn clean package
+
+nohup java -jar /home/dulle2/microflow/target/microflow-1.0-SNAPSHOT-jar-with-dependencies.jar & 
+
+
