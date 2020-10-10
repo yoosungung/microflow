2154783dd24cdf4aa039c1451dda167eff833bf0
eric yoo <yoosungung@users.noreply.github.com>
2020-10-10 18:54:16 +0900

    1010.1

--- src/main/java/io/berry/microflow/service/impl/UserManagerServiceImpl.java
+++ src/main/java/io/berry/microflow/service/impl/UserManagerServiceImpl.java
@@ -14,22 +14,26 @@ public class UserManagerServiceImpl extends JdbcManagerServiceImpl implements Us
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(UserManagerServiceImpl.class);
 
-	private final BaseAuth fauth;
+	private final JDBCAuth fauth;
 	private final String fpasswd_salt;
 	
 	public UserManagerServiceImpl(JsonObject conf) {
 		super(conf);
 	}
 
+	public void setAuth(JDBCAuth auth) {
+		fauth = auth;
+	}
+
 	private String getPasswdSalt() {
 		if(fpasswd_salt == null) {
-			fpasswd_salt = "";
+			fpasswd_salt = fauth.generatedSalt();
 		}
 		return fpasswd_salt;
 	}
 
-	private String getPasswdEncord(String raw_passwd, String passwd_slat) {
-		return "";
+	private String getPasswdHsah(String raw_passwd, String passwd_salt) {
+		return fauth.computedHash(raw_passwd, passwd_salt);
 	}
 
 	@Override
@@ -78,11 +82,14 @@ public class UserManagerServiceImpl extends JdbcManagerServiceImpl implements Us
 				return;
 			}
 			final SQLConnection conn = ar.result();
+			final Sting passwd_salt = getPasswordSalt();
+			final Sting passwd_hash = getPasswordHash(body.getString("password"), passwd_salt);
 			
 			JsonArray params = new JsonArray();
 			String sql = "insert into user (username, password, password_salt) values (?, ?, ?) ";
 			params.add(body.getString("username"))
-				.add(body.getString("password"));
+				.add(passwd_hash)
+				.add(passwd_salt);
 			conn.updateWithParams(sql, params, rs -> {
 				conn.close(done -> {
 					   if (done.failed()) {
@@ -102,21 +109,130 @@ public class UserManagerServiceImpl extends JdbcManagerServiceImpl implements Us
 	@Override
 	public void getUser(String username, OperationRequest context,
 			Handler<AsyncResult<OperationResponse>> resultHandler) {
-		// TODO Auto-generated method stub
+
+		this.getConnection(ar -> {
+			if(ar.failed()) {
+				LOGGER.error("getUser", ar.cause());
+				resultHandler.handle(Future.failedFuture(ar.cause()));
+				return;
+			}
+			final SQLConnection conn = ar.result();
+			JsonArray params = new JsonArray();
+			String sql = "select username, password from user where username = ? ";
+			params.add(username);
+
+			conn.queryWithParams(sql, params, res -> {
+				conn.close(done -> {
+					   if (done.failed()) {
+						   throw new RuntimeException(done.cause());
+					   }
+					});
+				if(res.failed()) {
+					LOGGER.error("getUser", res.cause());
+					resultHandler.handle(Future.failedFuture(ar.cause()));
+					return;
+				}
+				if(res.result().getNumRows() == 0) {					
+					resultHandler.handle(Future.succeededFuture(new OperationResponse().setStatusMessage("404")));
+				} else {
+					resultHandler.handle(Future.succeededFuture(OperationResponse.completedWithJson(new JsonArray(res.result().getRows()))));
+				}
+			});
+		});
 
 	}
 
 	@Override
 	public void updateUser(String username, JsonObject body, OperationRequest context,
 			Handler<AsyncResult<OperationResponse>> resultHandler) {
-		// TODO Auto-generated method stub
+
+		this.getConnection(ar -> {
+			if(ar.failed()) {
+				LOGGER.error("updateUser", ar.cause());
+				resultHandler.handle(Future.failedFuture(ar.cause()));
+				return;
+			}
+			final SQLConnection conn = ar.result();
+			final String passwd_salt = getPasswordSalt();
+			final Sting passwd_hash = getPasswordHash(body.getString("password"), passwd_salt);
+			
+			JsonArray params = new JsonArray();
+			String sql = "update user set password = ?, password_salt = ?  where username = ? ";
+			params.add(passwd_hash)
+				.add(passwd_salt)
+				.add(username);
+			conn.updateWithParams(sql, params, rs -> {
+				conn.close(done -> {
+					   if (done.failed()) {
+						   throw new RuntimeException(done.cause());
+					   }
+					});
+				if(rs.failed()) {
+					LOGGER.error("updateNode", rs.cause());
+					resultHandler.handle(Future.failedFuture(rs.cause()));
+				} else {
+					if(rs.result().getUpdated() == 0) {					
+						resultHandler.handle(Future.succeededFuture(new OperationResponse().setStatusMessage("404")));
+					} else {
+						resultHandler.handle(Future.succeededFuture(OperationResponse.completedWithJson(body)));
+					}				
+				}
+			});
+		});	
 
 	}
 
 	@Override
 	public void deleteUser(String username, OperationRequest context,
 			Handler<AsyncResult<OperationResponse>> resultHandler) {
-		// TODO Auto-generated method stub
+
+		this.getConnection(ar -> {
+			if(ar.failed()) {
+				LOGGER.error("deletUser", ar.cause());
+				resultHandler.handle(Future.failedFuture(ar.cause()));
+				return;
+			}
+			final SQLConnection conn = ar.result();
+			String sql;
+			JsonArray params = new JsonArray()add(username);
+			
+			sql = "delete from roles_perms where role in (select role from user_roles where username = ? ";
+			conn.updateWithParams(sql, params, rs -> {
+				if(rs.failed()) {
+					LOGGER.error("deletUser - roles_perms", rs.cause());
+					resultHandler.handle(Future.failedFuture(rs.cause()));
+				}
+			});
+
+			sql = "delete from user_roles where username = ? ";
+			conn.updateWithParams(sql, params, rs -> {
+				if(rs.failed()) {
+					LOGGER.error("deletUser - user_roles", rs.cause());
+					resultHandler.handle(Future.failedFuture(rs.cause()));
+				}
+			});
+
+			sql = "delete from user where username = ? ";
+			conn.updateWithParams(sql, params, rs -> {
+				if(rs.failed()) {
+					LOGGER.error("deletUser", rs.cause());
+					resultHandler.handle(Future.failedFuture(rs.cause()));
+				} else {
+					if(rs.result().getUpdated() == 0) {					
+						resultHandler.handle(Future.succeededFuture(new OperationResponse().setStatusMessage("404")));
+					} else {
+						resultHandler.handle(Future.succeededFuture(OperationResponse.completedWithJson(new JsonObject().put("delete","OK"))));
+					}				
+				}
+			});
+
+			conn.close(done -> {
+				if (done.failed()) {
+					throw new RuntimeException(done.cause());
+				}
+			});
+
+		});	
 
 	}
 
