5737fdeb021755831479e4325bbc959682f159c4
yoosungung <dulle2@gmail.com>
2020-10-10 08:14:04 +0000

    1010

--- src/main/java/io/berry/microflow/service/impl/UserManagerServiceImpl.java
+++ src/main/java/io/berry/microflow/service/impl/UserManagerServiceImpl.java
@@ -13,24 +13,90 @@ import io.vertx.ext.web.api.OperationResponse;
 public class UserManagerServiceImpl extends JdbcManagerServiceImpl implements UserManagerService {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(UserManagerServiceImpl.class);
+
+	private final BaseAuth fauth;
+	private final String fpasswd_salt;
 	
 	public UserManagerServiceImpl(JsonObject conf) {
 		super(conf);
-		// TODO Auto-generated constructor stub
+	}
+
+	private String getPasswdSalt() {
+		if(fpasswd_salt == null) {
+			fpasswd_salt = "";
+		}
+		return fpasswd_salt;
+	}
+
+	private String getPasswdEncord(String raw_passwd, String passwd_slat) {
+		return "";
 	}
 
 	@Override
 	public void getUserList(String username, OperationRequest context,
 			Handler<AsyncResult<OperationResponse>> resultHandler) {
-		// TODO Auto-generated method stub
+		
+		this.getConnection(ar -> {
+			if(ar.failed()) {
+				LOGGER.error("getUserList", ar.cause());
+				resultHandler.handle(Future.failedFuture(ar.cause()));
+				return;
+			}
+			final SQLConnection conn = ar.result();
+			JsonArray params = new JsonArray();
+			String sql = "select username, password from user where username like ? ";
+			params.add(username);
 
+			conn.queryWithParams(sql, params, res -> {
+				conn.close(done -> {
+					   if (done.failed()) {
+						   throw new RuntimeException(done.cause());
+					   }
+					});
+				if(res.failed()) {
+					LOGGER.error("getUserList", res.cause());
+					resultHandler.handle(Future.failedFuture(ar.cause()));
+					return;
+				}
+				if(res.result().getNumRows() == 0) {					
+					resultHandler.handle(Future.succeededFuture(OperationResponse.completedWithJson(new JsonArray())));
+				} else {
+					resultHandler.handle(Future.succeededFuture(OperationResponse.completedWithJson(new JsonArray(res.result().getRows()))));
+				}
+			});
+		});
 	}
 
 	@Override
 	public void createUser(String username, JsonObject body, OperationRequest context,
 			Handler<AsyncResult<OperationResponse>> resultHandler) {
-		// TODO Auto-generated method stub
 
+		this.getConnection(ar -> {
+			if(ar.failed()) {
+				LOGGER.error("createUser", ar.cause());
+				resultHandler.handle(Future.failedFuture(ar.cause()));
+				return;
+			}
+			final SQLConnection conn = ar.result();
+			
+			JsonArray params = new JsonArray();
+			String sql = "insert into user (username, password, password_salt) values (?, ?, ?) ";
+			params.add(body.getString("username"))
+				.add(body.getString("password"));
+			conn.updateWithParams(sql, params, rs -> {
+				conn.close(done -> {
+					   if (done.failed()) {
+						   throw new RuntimeException(done.cause());
+					   }
+					});
+				if(rs.failed()) {
+					LOGGER.error("createUser", rs.cause());
+					resultHandler.handle(Future.failedFuture(rs.cause()));
+				} else {
+					resultHandler.handle(Future.succeededFuture(OperationResponse.completedWithJson(body)));				
+				}
+			});
+		});
 	}
 
 	@Override
